cc2483841817d9f4245782aa2d17e40e5c3625be
Roberto Ciatti <roberto.ciatti@treuropa.com>
2019-03-04 18:37:42 +0100

    WIP: working on REST server

--- src/main/java/com/timequarter/sat/TowerCtrlVerticle.java
+++ src/main/java/com/timequarter/sat/TowerCtrlVerticle.java
@@ -1,17 +1,26 @@
 package com.timequarter.sat;
 
 import io.vertx.core.AbstractVerticle;
-/**
- * Hello world!
- *
- */
+import io.vertx.core.Future;
+
 public class TowerCtrlVerticle extends AbstractVerticle
 {
   @Override
-  public void start(){
-    vertx.createHttpServer()
-        .requestHandler(req -> req.response().end("Hi from your Download TowerControl!!!"))
-        .listen(8080);
+  public void start(Future<Void> startFuture) throws Exception {
+    Future<Void> startSequence = prepareRestServer();
+    startSequence.setHandler(startFuture.completer());
   }
+
+  private Future<Void> prepareRestServer(){
+    Future<Void> future = Future.future();
+
+    return future;
+  }
+//  @Override
+//  public void start(){
+//    vertx.createHttpServer()
+//        .requestHandler(req -> req.response().end("Hi from your Download TowerControl!!!"))
+//        .listen(8080);
+//  }
 }
 
